bfce6e3dd6093b004dfd08a47c12930b0642c167
Harsh Yadav <hy945196@gmail.com>
2023-12-10 16:02:15 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,83 @@ var express = require('express');
 var router = express.Router();
 let userModel = require('./users');
 const passport = require('passport');
+const upload = require("./multer")
 
 // user is login through this.
 const localStrategy = require("passport-local");
-passport.authenticate(new localStrategy(userModel.authenticate() ));
+passport.use(new localStrategy(userModel.authenticate() ));
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', {nav:false});
 });
 
-router.get('/profile',isloggedIn,function(req, res, next) {
-  res.send('welcome to profile')
+router.get('/register', function(req, res, next) {
+  res.render('index' , {nav:false});
+});
+
+router.get('/profile' ,async function(req, res, next) {
+  const user = await userModel.findOne({username:req.session.passport.user});
+  res.render("profile",{ user , nav: true});
+});
+
+router.get('/add' ,async function(req, res, next) {
+  const user = await userModel.findOne({username:req.session.passport.user});
+  res.render("add",{ user , nav: true});
+});
+
+router.get('/edit' ,async function(req, res, next) {
+  const user = await userModel.findOne({username:req.session.passport.user});
+  res.render("edit",{ user , nav: true});
+});
+
+router.post('/fileupload',isloggedIn,upload.single("image") ,async function(req, res,next) {
+  // res.send('uploaded');
+ const user = await userModel.findOne({username:req.session.passport.user});
+ user.profileImage = req.file.filename;
+ await user.save();
+ res.redirect("/profile")
 });
 
 router.post('/register',function(req,res){
-  const userData = {
+  const userData = new userModel ({
     username: req.body.username,
     email: req.body.email,
     fullname: req.body.fullname,
-  };
+    password:req.body.password,
+  })
+
+  userModel.register(userData , req.body.password)
+  .then(function(){
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/profile")
+    })
+  })
 })
 
-router.post('/login',passport.authenticate('local',{
+router.post('/',
+passport.authenticate('local',{
   successRedirect:'/profile',
   failureRedirect:'/',
-}),function(req,res){
+}),
+function(req,res){
+  res.render('login');
 });
+
+// router.get("/login",function(req,res){
+//   res.render('login', { title: 'Express' });
+// })
 
 router.get('/logout',function(req,res,next){
   req.logOut(function(err){
-   if(err) {return next(err)};
+   if(err) {return next(err);}
    res.redirect('/');
   })
 })
 
-function isloggedIn(req,res){
-  if(req.isAuthenticated()) return next();
+function isloggedIn(req,res,next){
+  if(req.isAuthenticated()) {
+    return next();
+  }
   res.redirect('/');
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,47 @@
-const mongoose = require("mongoose");
+var mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017//Pinterest");
+mongoose.connect("mongodb://127.0.0.1:27017/pin");
 
-const userSchema = new mongoose.Schema({
-  username:{
-    type:String,
-    require:true,
-    unique:true,
-  },
-  password:{
-   type:String,
-   require:true,
-  },
-  // posts:[{
-  //  type:mongoose.Schema.Types.ObjectId,
-  //  ref:'Post'
-  // }],
+const userSchema = mongoose.Schema({
+  // username:{
+  //   type:String,
+  //   require:true,
+  //   unique:true,
+  // },
+  // password:{
+  //  type:String,
+  //  require:true,
+  // },
+  // // posts:[{
+  // //  type:mongoose.Schema.Types.ObjectId,
+  // //  ref:'Post'
+  // // }],
 
-  dp:{
-    type:String,
-  },
-  email:{
-    type:String,
-    require:true,
-    unique:true,
-  },
-  fullname:{
-    type:String,
-    require:true,
+  // dp:{
+  //   type:String,
+  // },
+  // email:{
+  //   type:String,
+  //   require:true,
+  //   unique:true,
+  // },
+  // fullname:{
+  //   type:String,
+  //   require:true,
+  // }
+  username:String,
+  fullname:String,
+  email:String,
+  password:String,
+  profileImage:String,
+  boards:{
+    type: Array,
+    default:[]
   }
 });
 
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model('user',userSchema);
